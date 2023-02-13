dbba632f7088b7909911590ac2797a9b3c69e7b0
Juul <31242537+JuulH@users.noreply.github.com>
2023-02-13 15:28:23 +0100

    Setup of object manipulation

--- canvas.js
+++ canvas.js
@@ -1,4 +1,11 @@
-const canvas = new fabric.Canvas('canvas');
+const pageWidth  = Math.max(document.documentElement.clientWidth,  window.innerWidth  || 0);
+const pageHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);  
+
+const canvas = new fabric.Canvas('canvas', {
+    width: pageWidth,
+    height: pageHeight
+});
+
 const rect = new fabric.Rect({
     top: 100,
     left: 100,
@@ -6,4 +13,5 @@ const rect = new fabric.Rect({
     height: 70,
     fill: 'red',
 });
+
 canvas.add(rect);
