cb8de9cb603f5186a0f32100939493bde07b1dc2
Juul <31242537+JuulH@users.noreply.github.com>
2023-03-07 17:56:19 +0100

    Toolbar

--- canvas.js
+++ canvas.js
@@ -1,34 +1,37 @@
+const DOMCanvas = document.getElementById('canvas');
+const toolbar = document.getElementById('toolbar');
+
 const pageWidth  = Math.max(document.documentElement.clientWidth,  window.innerWidth  || 0);
 const pageHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);  
 
+const canvasWidth  = pageWidth - toolbar.offsetWidth - parseFloat(getComputedStyle(toolbar)['margin-left']);
+const canvasHeight = pageHeight;  
+
 const canvas = new fabric.Canvas('canvas', {
-    width: pageWidth,
-    height: pageHeight
+    width: canvasWidth,
+    height: canvasHeight
 });
 
-const rect = new fabric.Rect({
-    top: 100,
-    left: 100,
-    width: 60,
-    height: 70,
-    fill: 'red',
-});
 
-canvas.add(rect);
-
-window.addEventListener('mousedown', (event) => {
+function AddRect() {
     let rect = new fabric.Rect({
-        top: event.offsetY,
-        left: event.offsetX,
-        width: 50,
-        height: 50,
+        width: 100,
+        height: 100,
+        originX: 'center',
+        originY: 'center',
+        top: canvas.height / 2,
+        left: canvas.width / 2,
         fill: 'red',
     })
     canvas.add(rect);
-})
+}
 
-canvas.add(new fabric.IText('Tap and Type', { 
-    fontFamily: 'Comic Sans MS',
-    left: 100, 
-    top: 250 ,
-  }));
+function AddText() {
+    canvas.add(new fabric.IText('Tap and Type', { 
+        fontFamily: 'Comic Sans MS',
+            originX: 'center',
+            originY: 'center',
+            left: canvas.width / 2,
+            top: canvas.height / 2,
+    }));
+}
